5bdb032f3a28cebf3bd66533fbe460bea097e69f
Aman <amanxyz.10may@gmail.com>
2026-04-24 11:35:50 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,5 @@
+let marks=[96,98,97,95];
+let heroes=["luffy",19,"Zoro",21];
+console.log(marks);
+//console.log(marks.length); for printing length
+console.log("the element stored at index 0 is" ,marks[0]);
